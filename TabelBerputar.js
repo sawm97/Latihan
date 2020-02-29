cbebfb12ff65ffa38cf48b3c7a9fa91810959029
sawm97 <sriariefwibawamukti@gmail.com>
2020-02-29 09:23:31 +0700

    Render Tabel

--- TabelBerputar.js
+++ TabelBerputar.js
@@ -1,6 +1,30 @@
-var arrNum = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+var arrNum = [[1,2,3,4,0],[5,6,7,8,0],[9,10,11,12,0],[13,14,15,16,0],[0,0,0,0,0]]
 
-var arrHor = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+var arrHor = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
-var arrVer = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+var arrVer = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
+var fnRenderList = (data) => {
+    var resProduct = data.map((obj) => {
+        if (obj[0] == 0){
+            return `<tr>
+                <td><input type="button" value="sort"></td>
+                <td><input type="button" value="sort"></td>
+                <td><input type="button" value="sort"></td>
+                <td><input type="button" value="sort"></td>
+                <td></td>
+            </tr>`
+        } else {
+            return `<tr>
+                <td><center>${obj[0]}</center></td>
+                <td><center>${obj[1]}</center></td>
+                <td><center>${obj[2]}</center></td>
+                <td><center>${obj[3]}</center></td>
+                <td><input type="button" value="sort"></td>
+            </tr>`
+        }
+    })
+    document.getElementById("render").innerHTML = resProduct.join("")
+}
+
+fnRenderList(arrNum)
